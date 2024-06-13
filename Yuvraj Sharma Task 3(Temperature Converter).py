1e68f0dca087c9a9fe7ea055bd9cf5e21886346a
yuvraj9008 <growwise.yuvrajsharma.h@gmail.com>
2024-06-13 12:10:00 +0530

    Add files via upload

---
+++ Yuvraj Sharma Task 3(Temperature Converter).py
@@ -0,0 +1,8 @@
+# Temperature in celsius degree
+celsius = float(input("Enter temperature in celsius: "))
+# Converting the temperature to
+# fehrenheit using the formula
+fahrenheit = (celsius * 1.8) + 32
+ # printing the result
+print(str(celsius )+ " degree Celsius is equal to " + str  (fahrenheit) +
+" degree Fahrenheit.")
